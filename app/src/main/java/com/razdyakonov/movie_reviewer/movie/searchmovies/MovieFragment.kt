6e6e9175db92642009715bc73ee1b6cb29e2c70a
razdyakonov viktor <razvical@mail.ru>
2021-07-25 21:48:02 +0300

    Fix fragment implementation layout and recycler height

--- app/src/main/java/com/razdyakonov/movie_reviewer/movie/searchmovies/MovieFragment.kt
+++ app/src/main/java/com/razdyakonov/movie_reviewer/movie/searchmovies/MovieFragment.kt
@@ -1,9 +1,7 @@
 package com.razdyakonov.movie_reviewer.movie.searchmovies
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,15 +10,7 @@ import com.razdyakonov.movie_reviewer.R
 import com.razdyakonov.movie_reviewer.movie.model.Movie
 import com.razdyakonov.movie_reviewer.movie.recyclerview.MovieAdapter
 
-class MovieFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie, container, false)
-    }
+class MovieFragment : Fragment(R.layout.fragment_movie) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val context = requireContext()
@@ -28,19 +18,26 @@ class MovieFragment : Fragment() {
 
         val recyclerView: RecyclerView = view.findViewById(R.id.movie_rv)
 
-        val fakeMovieList = listOf(
-            Movie(id = 0, name = "Avengers"),
-            Movie(id = 1, name = "Superman"),
-            Movie(id = 2, name = "Ironman"),
-            Movie(id = 3, name = "Matrix"),
-        )
+        val fakeMovieList = mutableListOf<Movie>()
+
+        for (i in 1..10) {
+            fakeMovieList.addAll(
+                listOf(
+                    Movie(id = 0, name = "Avengers"),
+                    Movie(id = 1, name = "Superman"),
+                    Movie(id = 2, name = "Ironman"),
+                    Movie(id = 3, name = "Matrix"),
+                )
+            )
+        }
 
         recyclerView.layoutManager = LinearLayoutManager(context).apply {
             orientation = LinearLayoutManager.VERTICAL
         }
 
-        val adapter = MovieAdapter(layoutInflater)
-        adapter.submitList(fakeMovieList)
+        val adapter = MovieAdapter(layoutInflater).apply {
+            submitList(fakeMovieList)
+        }
         recyclerView.adapter = adapter
     }
 }
